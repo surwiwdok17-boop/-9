78c37fe024b90a1ef69b9757adcb64c30687bd83
Cereruss <nezurona@gmail.com>
2025-12-12 15:13:51 +0200

    Delete lab9/lab9 directory

--- lab9/lab9/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .home import home_bp
-from .shop import shop_bp
-from .feedback import feedback_bp
-from .admin import admin_bp
-from .api import api_bp   # новий імпорт
-
-# Єдиний список блупринтів
-blueprints = [home_bp, shop_bp, feedback_bp, api_bp, admin_bp]
--- lab9/lab9/routes/demo.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-demo_bp = Blueprint("demo", __name__)
-
-@demo_bp.route("/api-demo")
-def api_demo():
-    return render_template("api-demo.html")
